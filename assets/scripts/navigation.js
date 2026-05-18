58a47509f8cb4c9864314224bad54f650576aceb
visstudi <dungeongachi6hotloads@gmail.com>
2026-05-18 22:50:08 +0400

    Fix routes page

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -1,13 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navButtons = document.querySelectorAll('nav button');
-    const pages = [
-        'traffic.html', 'routes.html', 'requests.html', 
-        'jobs.html', 'journal.html', 'users.html'
-    ];
+document.addEventListener("DOMContentLoaded", () => {
+  const navButtons = document.querySelectorAll("nav button");
+  const pages = [
+    "traffic.html",
+    "routes.html",
+    "requests.html",
+    "jobs.html",
+    "journal.html",
+    "users.html",
+  ];
 
-    navButtons.forEach((btn, index) => {
-        btn.onclick = () => {
-            if (pages[index]) window.location.href = pages[index];
-        };
-    });
+  navButtons.forEach((btn, index) => {
+    btn.onclick = () => {
+      if (pages[index]) window.location.href = pages[index];
+    };
+  });
 });
